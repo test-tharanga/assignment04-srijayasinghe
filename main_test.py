faaa60658467ff6a086f65e7f07a698beeab35b5
srijayasinghe84 <tsrijayasi@wiley.com>
2023-12-15 08:54:36 +0000

    update3

--- main_test.py
+++ main_test.py
@@ -3,4 +3,5 @@ import main
 def test(capsys):
     main.calculate()
     captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+   # assert captured.out == "Total: 78.75\n"
+    assert captured.out == "Total: 47.25\n"
